b5ce10a304e406b2e70596b9ba8ac280628228c1
Amine A <15179425+AmineI@users.noreply.github.com>
2021-10-19 08:49:03 +0200

    Add database error page middleware

--- Source/WebApp-IdentityProvider-FIDO2/Program.cs
+++ Source/WebApp-IdentityProvider-FIDO2/Program.cs
@@ -6,6 +6,8 @@ using WebApp_IdentityProvider_MFA.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
@@ -37,7 +39,11 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
