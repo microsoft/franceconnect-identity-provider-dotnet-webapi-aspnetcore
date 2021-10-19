676c5c8ed38ccba6c672b96b0e264c6c229a682d
Amine A <15179425+AmineI@users.noreply.github.com>
2021-10-19 08:49:05 +0200

    Add a "back to FranceConnect" button on the login screen when invoked from OIDC
    As recommended in FranceConnect's documentation

--- Source/WebApp-IdentityProvider-MFA/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Source/WebApp-IdentityProvider-MFA/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
+using IdentityServer4.Models;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,20 +15,23 @@ namespace WebApp_IdentityProvider_MFA.Areas.Identity.Pages.Account
     public class LoginModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IIdentityServerInteractionService _interaction;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<ApplicationUser> signInManager,
+            IIdentityServerInteractionService interaction, ILogger<LoginModel> logger)
         {
             _signInManager = signInManager;
+            _interaction = interaction;
             _logger = logger;
         }
 
+        public bool IsFranceConnectLoginRequest { get; set; }
 
         [BindProperty]
         public InputModel Input { get; set; }
 
         public string ReturnUrl { get; set; }
-
         [TempData]
         public string ErrorMessage { get; set; }
 
@@ -52,14 +57,26 @@ namespace WebApp_IdentityProvider_MFA.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
-
-            ReturnUrl = returnUrl;
+            ReturnUrl = returnUrl ?? Url.Content("~/");
+            // check if we are in the context of an authorization request
+            IsFranceConnectLoginRequest = (await _interaction.GetAuthorizationContextAsync(ReturnUrl) != null);
         }
+        public async Task<IActionResult> OnGetCancelLoginAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl ?? Url.Content("~/");
+            AuthorizationRequest context = await _interaction.GetAuthorizationContextAsync(ReturnUrl);
+            if (context != null)
+            {
+                await _interaction.DenyAuthorizationAsync(context, AuthorizationError.AccessDenied);
+                // this will send back an access denied OIDC error response to FranceConnect
+                return Redirect(ReturnUrl);
+            }
+            return Page();
 
+        }
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            ReturnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
                 // TODO :This doesn't count login failures towards account lockout
@@ -68,11 +85,11 @@ namespace WebApp_IdentityProvider_MFA.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return LocalRedirect(ReturnUrl);
                 }
                 if (result.RequiresTwoFactor)
                 {
-                    return RedirectToPage("./Login2FaChoice", new { ReturnUrl = returnUrl, Input.RememberMe });
+                    return RedirectToPage("./Login2FaChoice", new { ReturnUrl, Input.RememberMe });
                 }
                 if (result.IsLockedOut)
                 {
