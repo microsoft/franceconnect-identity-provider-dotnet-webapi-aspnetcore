2033cf4d88a3c63f7ddc84b0ed5981075394535c
Amine A <15179425+AmineI@users.noreply.github.com>
2021-10-19 08:49:02 +0200

    Create a new ASP.NET Core 6 MVC Project and Scaffold Identity
    Update Program.cs
    
    a
    
    Create from scratch

---
+++ Source/WebApp-IdentityProvider-FIDO2/Areas/Identity/Pages/Account/ResetPasswordConfirmation.cshtml.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_IdentityProvider_MFA.Areas.Identity.Pages.Account
+{
+
+    [AllowAnonymous]
+    public class ResetPasswordConfirmationModel : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Source/WebApp-IdentityProvider-FIDO2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_IdentityProvider_MFA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
